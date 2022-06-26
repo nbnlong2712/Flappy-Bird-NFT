225ffe871f08f43fb8a24b309f13ec9112fca23d
nbnlong2712 <nhatlonghn1203@gmail.com>
2022-06-26 11:46:49 +0700

    Long: add Safechain, connect metamask, setup ui

---
+++ Assets/Scripts/WalletAccount.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WalletAccount : MonoBehaviour
+{
+    public Text myAccount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAccount.text = PlayerPrefs.GetString("Account");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
